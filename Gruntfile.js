a7730f3c337ff551339fe53cecc6032b92cd8a5b
Anthony Gilardi <acgilardi@gmail.com>
2015-04-22 06:51:49 -0400

    add base structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,52 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-
+        copy: {
+            init: {
+                files: {
+                    src: [ '**' ],
+                    dest: 'build/vendor/',
+                    cwd: 'vendor',
+                    expand: true
+                }
+            },
+            build: {
+                files: [
+                    {
+                        src: [ '**' ],
+                        dest: 'build/',
+                        cwd: 'src',
+                        expand: true
+                    }
+                ]
+            }
+        },
         bower: {
             install: {
                 options: {
-                    targetDir: 'vendor',
+                    targetDir: 'src/vendor',
                     layout: 'byComponent'
                 }
+            }
+        },
+        watch: {
+            dev: {
+                files: ['src/**/*.*'],
+                tasks: ['build']
+            },
+            options: {
+                livereload: true
             }
         }
     });
 
     // Load NPM tasks
     grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default Task
     grunt.registerTask('default', ['bower']);
+    grunt.registerTask('build', ['copy:build']);
+    grunt.registerTask('serve', ['build', 'watch']);
 };
